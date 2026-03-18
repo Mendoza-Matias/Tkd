a718c3db5874d1b7423f7db715f92c798d5f7607
Mendoza Matias <mendoza.e.matias@gmail.com>
2026-03-18 12:05:12 -0300

    configuration of workflow and swagger

---
+++ src/test/java/com/mmendoza/tkd/TkdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmendoza.tkd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
